4580169c2f514faad5ee642df835054d9c952aaa
Awais <awais.aamir@outlook.com>
2026-09-09 12:52:34 +0100

    Initialised Project

---
+++ src/main/java/com/example/osrs_market_tracker/OsrsMarketTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.osrs_market_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsMarketTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsMarketTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/osrs_market_tracker/OsrsMarketTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osrs_market_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsMarketTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
